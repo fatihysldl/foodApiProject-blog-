03d565e06b6a24d08416cfadfe460d88da163497
Muhammed Fatih Yeşildal <fatihgithub@gmail.com>
2024-06-19 15:49:09 +0300

    recipe table was created

---
+++ api/entityLayer/concrete/recipeTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entityLayer.concrete
+{
+    public class recipeTable
+    {
+        [Key]
+        public int RecipeId { get; set; }
+        [StringLength(20)]
+        public string RecipeHeader { get; set; }
+        [MaxLength]
+        public string RecipeValue { get; set; }
+
+        [StringLength(100)]
+        public string RecipeIngredients { get; set; }
+
+        [StringLength(150)]
+        public string ımagePath { get; set; }
+        public List<postTable>? Posts { get; set; }
+    }
+}
